82e6e33c20c065b742f61b760ad6c7298cd59862
dylanyang <dylanyang@152.23.214.235>
2019-08-29 18:04:07 -0400

    Added comments explaining process

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -21,43 +21,54 @@ public class A1Novice {
 		//Create an array of integers with the amount of unique items each customer buys
 		int[] itemList = new int[totalCustomers];
 
-		
 		//Create a for loop to fill in the names of the customers and their shopping lists
 		for (int i = 0; i < totalCustomers; i++) {
 			
+			//Gets the customers first name and adds it to the first name list
 			String customerFirstName = scan.next();
 			firstNames[i] = customerFirstName;
 			
+			//Gets the customers last name and adds its to the last name list
 			String customerLastName = scan.next();
 			lastNames[i] = customerLastName;
 			
+			//Gets the amount of unique items each customer buys and adds it to the array itemList
 			int uniqueItems = scan.nextInt();
 			itemList[i] = uniqueItems;
 			
+			//Creates an array that holds the total price of items for each person
 			double[] itemTotal = new double[1];
 			
+			//Create a for loop to find the total prices for each persons items
 			for (int k = 0; k < uniqueItems; k++) {
+				
+				//An array for the amount of each item bought
 				double[] itemAmountArray = new double[uniqueItems];
 				
+				//An array for the price of each item bought
 				double[] itemPriceArray = new double[uniqueItems];
 				
+				//Gets the amount of each item bought and adds it to the array
 				double itemAmount = scan.nextDouble();
 				itemAmountArray[k] = itemAmount;
 				
+				//Gets the name of each item
 				String itemName = scan.next();
 				
+				//Gets the price of each item bought and adds it to the array
 				double itemPrice = scan.nextDouble();
 				itemPriceArray[k] = itemPrice;
 								
+				//Calculates the total costs for each person
 				double totalChange = itemAmountArray[k] * itemPriceArray[k];
-				
 				itemTotal[0] = itemTotal[0] + totalChange;
 
 			}
-						
+			
+			//Gets the first letter of each person's name
 			char firstChar = firstNames[i].charAt(0);
 			
-			
+			//Prints the output
 			System.out.println(firstChar + ". " + lastNames[i] + ": " + String.format("%.2f", itemTotal[0]));
 						
 			}
